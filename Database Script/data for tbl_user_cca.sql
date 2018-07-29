4c52d9d06257bea01d26a2737f66f5edaed5a11f
huiyannn <41505488+huiyannn@users.noreply.github.com>
2018-07-29 22:47:17 +0800

    Add files via upload

--- Database Script/data for tbl_user_cca.sql
+++ Database Script/data for tbl_user_cca.sql
@@ -2,10 +2,10 @@
 -- Query: SELECT * FROM narwhal.tbl_user_cca
 LIMIT 0, 1000
 
--- Date: 2018-07-29 22:42
+-- Date: 2018-07-29 22:46
 */
 INSERT INTO `tbl_user_cca` (`user_ccaID`,`userID`,`ccaID`,`quit`) VALUES (5,4,3,'Active');
-INSERT INTO `tbl_user_cca` (`user_ccaID`,`userID`,`ccaID`,`quit`) VALUES (7,7,3,'Inactive');
+INSERT INTO `tbl_user_cca` (`user_ccaID`,`userID`,`ccaID`,`quit`) VALUES (7,7,3,'Active');
 INSERT INTO `tbl_user_cca` (`user_ccaID`,`userID`,`ccaID`,`quit`) VALUES (8,1,3,'Active');
 INSERT INTO `tbl_user_cca` (`user_ccaID`,`userID`,`ccaID`,`quit`) VALUES (9,2,3,'Inactive');
 INSERT INTO `tbl_user_cca` (`user_ccaID`,`userID`,`ccaID`,`quit`) VALUES (10,3,3,'Inactive');
