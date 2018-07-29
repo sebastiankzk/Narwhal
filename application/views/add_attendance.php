023bd0316e7d6083138fac42b973c3bfa5993836
Diliyz <144315F@sit.nyp.edu.sg>
2018-07-29 20:02:20 +0800

    Fixed Mobile menu not showing

--- application/views/add_attendance.php
+++ application/views/add_attendance.php
@@ -34,7 +34,7 @@
  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
     <a class="navbar-brand" href="<?=base_url();?>index.php">NYP CCA Portal</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="index.php#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
