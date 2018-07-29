f822b7251c92820dc92171192337281913bb52cd
huiyannn <41505488+huiyannn@users.noreply.github.com>
2018-07-30 00:40:31 +0800

    Add files via upload
    data for event in sql

---
+++ Database Script/data for event.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM narwhal.event
+LIMIT 0, 1000
+
+-- Date: 2018-07-30 00:40
+*/
+INSERT INTO `event` (`eventID`,`name`,`description`,`venue`,`date`,`time`) VALUES (1,'Sport Camp','Camp for sport CCA','NYP Block A','2018-08-11','16:03:00');
+INSERT INTO `event` (`eventID`,`name`,`description`,`venue`,`date`,`time`) VALUES (2,'FMO','FMO','Block A','2018-12-12','14:22:00');
+INSERT INTO `event` (`eventID`,`name`,`description`,`venue`,`date`,`time`) VALUES (3,'CCA Camp','CCA camp','NYP','2019-01-11','14:01:00');
