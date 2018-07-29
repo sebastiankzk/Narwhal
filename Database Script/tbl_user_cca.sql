a243e128cde55d2374fdf284a773b8c3eace12a1
huiyannn <41505488+huiyannn@users.noreply.github.com>
2018-07-29 22:44:43 +0800

    Add files via upload

--- Database Script/tbl_user_cca.sql
+++ Database Script/tbl_user_cca.sql
@@ -1,8 +1,7 @@
-CREATE TABLE `tbl_user_cca` (
+SELECT * FROM narwhal.tbl_user_cca;CREATE TABLE `tbl_user_cca` (
   `user_ccaID` int(11) NOT NULL,
   `userID` int(11) DEFAULT NULL,
   `ccaID` int(11) DEFAULT NULL,
-  `quit` varchar(45) DEFAULT NULL,
+  `quit` varchar(45) DEFAULT 'Active',
   PRIMARY KEY (`user_ccaID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SELECT * FROM narwhal.tbl_user_cca;
