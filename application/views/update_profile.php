2b7f3112592a8f85f8fe51fd9517b97f7fca597c
MTKL1106 <39946669+MTKL1106@users.noreply.github.com>
2018-07-29 19:29:19 +0800

    Edited nav
    Edited nav

--- application/views/update_profile.php
+++ application/views/update_profile.php
@@ -86,7 +86,7 @@
     </h1>
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
-        <a href="index.html">Home</a>
+        <a href="<?php echo base_url('index.php'); ?>">Home</a>
       </li>
       <li class="breadcrumb-item">
        <a href="<?php echo base_url('index.php/Profile/'); ?>">Accounts</a>
--- application/views/view_profile.php
+++ application/views/view_profile.php
@@ -91,7 +91,7 @@
     </h1>
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
-        <a href="index.html">Home</a>
+        <a href="<?php echo base_url('index.php'); ?>">Home</a>
       </li>
       <li class="breadcrumb-item active">Account</li>
    </ol>
